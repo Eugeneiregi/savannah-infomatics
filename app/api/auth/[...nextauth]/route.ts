0a4e9c4829571895c28175c50236f8e9e8e6d5bc
Eugene <59908155+Eugeneiregi@users.noreply.github.com>
2024-05-29 03:26:38 -0700

    Refactored auth authoptions by removing types

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,49 @@
 import NextAuth from "next-auth";
-import type { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions: AuthOptions = {
+const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  // Add other options here, if necessary
 };
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
+
+
+
+// import NextAuth from "next-auth";
+// import { AuthOptions } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import GithubProvider from "next-auth/providers/github";
+
+// // interface AuthOptions {
+// //     providers: Array<any>;
+// //   }
+
+// export const authOptions: AuthOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+//     }),
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID as string,
+//       clientSecret: process.env.GITHUB_SECRET as string
+//     }),
+//   ],
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
