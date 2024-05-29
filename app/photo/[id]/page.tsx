5725f304f0dc820f0bf782248ae6925fa2b14180
Eugene <59908155+Eugeneiregi@users.noreply.github.com>
2024-05-29 01:21:28 -0700

    Added dynamic routing to fetch user album photo as per id and i have added some ui template design

--- app/photo/[id]/page.tsx
+++ app/photo/[id]/page.tsx
@@ -1,5 +1,17 @@
-"use client"
+"use client";
 
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogClose,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
@@ -7,6 +19,9 @@ import { useParams } from 'next/navigation';
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from '@/components/ui/button';
 import router from 'next/router';
+import Link from "next/link"
+import Header from '@/components/shared/Header';
+import Footer from '@/components/shared/Footer';
 
 interface Photo {
     id: number;
@@ -25,64 +40,107 @@ const PhotoPage = () => {
     useEffect(() => {
         setLoading(true);
         const fetchPhoto = async () => {
-          try {
-            const photoResponse = await axios.get<Photo>(`https://jsonplaceholder.typicode.com/photos/${id}`);
-            setPhoto(photoResponse.data);
-            setTitle(photoResponse.data.title)
-            setLoading(false);
-          } catch (err) {
-            setLoading(false);
-            console.error('Error fetching photo:', err);
-          }
+            try {
+                const photoResponse = await axios.get<Photo>(`https://jsonplaceholder.typicode.com/photos/${id}`);
+                setPhoto(photoResponse.data);
+                setTitle(photoResponse.data.title)
+                setLoading(false);
+            } catch (err) {
+                setLoading(false);
+                console.error('Error fetching photo:', err);
+            }
         };
         fetchPhoto();
-      }, [id]);
-    
-      const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    }, [id]);
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setTitle(e.target.value);
-      };
-    
-      const handleSubmit = async () => {
+    };
+    let router;
+    if (typeof window !== 'undefined') {
+        router = require('next/router').default;
+    }
+
+    const handleSubmit = async () => {
         setLoadingUpdate(true);
         try {
-          const response = await axios.patch(`https://jsonplaceholder.typicode.com/photos/${id}`, {
-            title: title,
-          });
-          setPhoto(response.data);
-          toast.success(`Title Updated to "${response?.data?.title}" successfully`)
-          setLoadingUpdate(false);
+            const response = await axios.patch(`https://jsonplaceholder.typicode.com/photos/${id}`, {
+                title: title,
+            });
+            setPhoto(response.data);
+            toast.success(`Title Updated to "${response?.data?.title}" successfully`)
+            setLoadingUpdate(false);
         } catch (error) {
-          setLoadingUpdate(false);
-          console.error('Error updating photo title:', error);
+            setLoadingUpdate(false);
+            console.error('Error updating photo title:', error);
         }
-      };
+    };
 
     return (
-        <div className="flex items-center justify-center min-h-screen">
-        <div className="max-w-md w-full p-8 bg-white rounded-lg shadow-lg">
-            <div className="h-64 mb-6">
-                <img className="object-cover w-full h-full" src={photo?.url} alt={photo?.title} />
+        <>
+            <Header />
+            <div className="flex items-center justify-center min-h-screen wrapper">
+                <div className="max-w-md w-full p-8 bg-white rounded-lg shadow-lg">
+                    <div className="h-74 mb-6">
+                        <img className="object-cover w-full h-full" src={photo?.url} alt={photo?.title} />
+                    </div>
+                    <div>
+                        <div className="font-bold text-xl mb-2">{photo?.title}</div>
+                        <Textarea value={title} onChange={handleTitleChange} />
+                    </div>
+                    <div className="flex flex-col space-x-2 pt-10 px-10 py-5">
+
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button className="hover:bg-blue-700 text-white font-bold rounded">Edit Profile</Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[425px] bg-slate-50 text-black">
+                                <DialogHeader>
+                                    <DialogTitle>Edit profile</DialogTitle>
+                                    <DialogDescription>
+                                        Make changes to your profile here. Click save when you're done.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <div className="grid gap-4 py-4">
+                                    <div>
+                                        <div className="font-bold text-xl mb-2">{photo?.title}</div>
+                                        <Textarea value={title} onChange={handleTitleChange} />
+                                    </div>
+                                    <div className="flex flex-col space-x-2 pt-10 px-10 py-5">
+                                        <Button
+                                            className=" hover:bg-blue-700 text-white font-bold rounded"
+                                            onClick={handleSubmit}
+                                            disabled={loadingUpdate}
+                                        >
+                                            {loadingUpdate ? 'Updating...' : 'Update'}
+                                        </Button>
+                                    </div>
+                                </div>
+                                <DialogFooter>
+                                    <DialogClose asChild>
+                                        <Button type="button" variant="secondary">
+                                            Close
+                                        </Button>
+                                    </DialogClose>
+                                    {/* <Button onClick={handleSubmit} disabled={loadingUpdate}>
+                                        {loadingUpdate ? 'Updating...' : 'Save changes'}
+                                    </Button> */}
+                                </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
+                        <Dialog>
+                            <DialogClose asChild>
+                                <Button type="button" variant="secondary" className="text-black font-bold px-10 py-5">
+                                    <Link href="/users">Start Again</Link>           
+                                </Button>
+                            </DialogClose>
+                        </Dialog>
+                    </div>
+                </div>
+
             </div>
-            <div>
-                <div className="font-bold text-xl mb-2">{photo?.title}</div>
-                <Textarea value={title} onChange={handleTitleChange} />
-            </div>
-            <div className="mt-6">
-                <button 
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-                    onClick={handleSubmit}
-                    disabled={loadingUpdate}
-                >
-                    {loadingUpdate ? 'Updating...' : 'Update'}
-                </button>
-            </div>
-            <div className="mt-6">
-                <Button onClick={() => router.push('/')}>
-                    Go Home
-                </Button>
-            </div>
-        </div>
-    </div>
+            <Footer />
+        </>
     )
 }
 
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -78,7 +78,7 @@ const UsersPage: React.FC = () => {
                 {loading ? (
                     <div>Loading...</div>
                 ) : (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 bg-white text-black">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 bg-white text-black rounded-full">
                         {userAlbumCounts.map(user => (
                             <div
                                 key={user.id}
@@ -86,7 +86,7 @@ const UsersPage: React.FC = () => {
                                 onClick={() => router.push(`/users/${user.id}`)}
                             >
                                
-                                <div className="px-6 py-4">
+                                <div className="px-10 py-7 hover:bg-white text-black">
                                     <div className="font-bold text-xl mb-2">{user.name}</div>
                                     <p className="text-gray-700 text-base">
                                         Albums: {user.albumCount}
@@ -95,7 +95,7 @@ const UsersPage: React.FC = () => {
                                 <div className="px-6 pt-4 pb-2">
                                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#user</span>
                                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#albums</span>
-                                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#details</span>
+                                    
                                 </div>
                             </div>
                         ))}
